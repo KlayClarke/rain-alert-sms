da2ea5406e759a37241bdf638b39bc4fda30b74e
Klay Clarke <klayaclarke@gmail.com>
2021-11-29 08:32:52 -0500

    use twilio to send sms notifications when 12 hour forecast predicts rain

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import requests
+from twilio.rest import Client
+
+# Find your Account SID and Auth Token at twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+account_sid = 'AC6441500d5adc9b8a0efb709d2d0a4c38'
+auth_token = 'bed7adec29e116248d5f51f6e5119ca7'
 
 api_key = 'a5562bcf068078abc0381db8dfc12bd6'
 
-latitude = 45.764042
-longitude = 4.835659
+latitude = 42.360081
+longitude = -71.058884
 
 parameters = {
     'lat': latitude,
@@ -21,9 +27,18 @@ weather_slice = hourly_data[:12]
 
 half_day_hourly_condition = []
 
+from_number = None
+to_number = None
+
 for data in weather_slice:
     half_day_hourly_condition.append(data['weather'][0]['id'])
     if data['weather'][0]['id'] < 700:
-        print('Bring an umbrella')
+        client = Client(account_sid, auth_token)
+        message = client.messages \
+            .create(
+            body="It's going to rain today. Remember to bring an umbrella!",
+            from_=f'{from_number}',
+            to=f'{to_number}'
+        )
+        print(message.status)
 
-print(half_day_hourly_condition)
