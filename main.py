ad7e60ab3380b365560cd2157e1aa1387db42d06
Klay Clarke <klayaclarke@gmail.com>
2021-11-29 09:18:58 -0500

    use twilio to send sms when 12-hour forecast predicts rain
    bug with twilio authentication / and https errors are preventing me from running this code again, but it sent one message earlier today (so it worked at least once)

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import os
 import requests
 from twilio.rest import Client
+from twilio.http.http_client import TwilioHttpClient
 
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
@@ -8,8 +10,8 @@ auth_token = 'bed7adec29e116248d5f51f6e5119ca7'
 
 api_key = 'a5562bcf068078abc0381db8dfc12bd6'
 
-latitude = 42.360081
-longitude = -71.058884
+latitude = 42.3601
+longitude = 71.0589
 
 parameters = {
     'lat': latitude,
@@ -30,15 +32,21 @@ half_day_hourly_condition = []
 from_number = None
 to_number = None
 
+will_rain = False
+
 for data in weather_slice:
     half_day_hourly_condition.append(data['weather'][0]['id'])
     if data['weather'][0]['id'] < 700:
-        client = Client(account_sid, auth_token)
-        message = client.messages \
-            .create(
-            body="It's going to rain today. Remember to bring an umbrella!",
-            from_=f'{from_number}',
-            to=f'{to_number}'
-        )
-        print(message.status)
+        will_rain = True
+
+if will_rain:
+    proxy_client = TwilioHttpClient(proxy={'http': os.environ['http_proxy'], 'https': os.environ['https_proxy']})
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+        .create(
+        body="It's going to rain today. Remember to bring an umbrella!",
+        from_=f'{from_number}',
+        to=f'{to_number}'
+    )
+    print(message.status)
 
