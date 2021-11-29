c23338d89be3e410636546bd9705988f3a94381a
Klay Clarke <klayaclarke@gmail.com>
2021-11-28 19:01:35 -0500

    openweather api request for Boston weather data

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import requests
+
 api_key = 'a5562bcf068078abc0381db8dfc12bd6'
+
+latitude = 42.360081
+longitude = -71.058884
+
+parameters = {
+    'lat': latitude,
+    'lon': longitude,
+    'appid': api_key
+}
+
+response = requests.get(url='https://api.openweathermap.org/data/2.5/onecall', params=parameters)
+response.raise_for_status()
+data = response.json()
+print(data)
